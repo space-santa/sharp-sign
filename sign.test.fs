bad106476ecf3f09024fd9701c0ea1c2aaf053b0
claus <claus.z@protonmail.com>
2018-03-16 21:15:42 +1100

    Add actual tests of sign code

--- sign.test.fs
+++ sign.test.fs
@@ -2,14 +2,15 @@ module SignTest
 
 open NUnit.Framework
 open FsUnit
+open SharpSign
 
 [<TestFixture>]
 type Test() =
 
     [<Test>] member x.
-        ``One is One`` () =
-            1 |> should equal 1
+        ``should be line length + 4`` () =
+            signWidth("abx") |> should equal 7
 
     [<Test>] member x.
-        ``One is not Two`` () =
-            1 |> should not' (equal 2)
+        ``should wrap line`` () =
+            wrapLine "abc" 8 |> should equal "| abc      |"
