a81397f780e62cfa75115fce49ecb8dae57e88c6
claus <claus.z@protonmail.com>
2018-03-16 15:40:20 +1100

    Upgrade bundle.sh to package.json

--- bundle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-fsharpc sign.fs
-mkbundle -o sign_pi --simple sign.exe --cross mono-5.10.0-raspbian-9-arm
-mkbundle -o sign_mac --simple sign.exe --cross mono-5.10.0-osx-10.7-x64
-mkbundle -o sign --simple sign.exe
-mkbundle -o sign_x64 --simple sign.exe --cross mono-5.10.0-ubuntu-12.04-x64
