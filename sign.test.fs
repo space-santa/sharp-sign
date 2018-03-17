bfef65ec7d247fe64c9770325eabe04fda318d20
claus <claus.z@protonmail.com>
2018-03-17 22:05:03 +1100

    Add test for splitTokenAtLimit

--- sign.test.fs
+++ sign.test.fs
@@ -8,9 +8,35 @@ open SharpSign
 type Test() =
 
     [<Test>] member x.
-        ``should be line length + 4`` () =
-            signWidth("abx") |> should equal 7
+        ``signWidth`` () =
+            StringOperations.signWidth("abx") |> should equal 7
+
 
     [<Test>] member x.
-        ``should wrap line`` () =
-            wrapLine "abc" 8 |> should equal "| abc      |"
+        ``wrapLine`` () =
+            StringOperations.wrapLine "abc" 8 |> should equal "| abc      |"
+
+[<TestFixture>]
+type TestSplitTokenAtLimit() =
+    let str = "abcdefqwerty"
+    [<Test>] member x.
+        ``no split`` () =
+            StringOperations.splitTokenAtLimit 13 str |> should equal [str]
+
+    [<Test>] member x.
+        ``still no split`` () =
+            StringOperations.splitTokenAtLimit 12 str |> should equal [str]
+
+    [<Test>] member x.
+        ``split last`` () =
+            StringOperations.splitTokenAtLimit 11 str |> should equal ["abcdefqwert"; "y"]
+
+    [<Test>] member x.
+        ``split first`` () =
+            StringOperations.splitTokenAtLimit 1 str |>
+                should equal ["a"; "b"; "c"; "d"; "e"; "f"; "q"; "w"; "e"; "r"; "t"; "y"]
+
+    [<Test>] member x.
+        ``split zero`` () =
+            (fun () -> StringOperations.splitTokenAtLimit 0 str |> ignore) |>
+                should throw typeof<System.ArgumentException>
