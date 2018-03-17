2a402091cbd2945b0cc6079b3e62daba1fd71770
claus <claus.z@protonmail.com>
2018-03-17 22:03:57 +1100

    Split sign.fs into modules

--- sign.fs
+++ sign.fs
@@ -2,88 +2,100 @@ module SharpSign
 
 open System
 
-let getConsoleWidth =
-    let p = new System.Diagnostics.Process();
-    p.StartInfo.FileName <- "tput"
-    p.StartInfo.Arguments <- "cols"
-    p.StartInfo.RedirectStandardOutput <- true
-    p.StartInfo.UseShellExecute <- false
-    p.Start() |> ignore
-    p.StandardOutput.ReadToEnd() |> int
+module ConsoleIO =
+    let getConsoleWidth =
+        let p = new System.Diagnostics.Process();
+        p.StartInfo.FileName <- "tput"
+        p.StartInfo.Arguments <- "cols"
+        p.StartInfo.RedirectStandardOutput <- true
+        p.StartInfo.UseShellExecute <- false
+        p.Start() |> ignore
+        p.StandardOutput.ReadToEnd() |> int
 
-let signWidth (line:String) =
-    line.Length + 4
 
-let maxLineLength =
-    getConsoleWidth - 4
+module StringOperations =
+    let signWidth (line:String) =
+        line.Length + 4
 
-let rec splitTokenAtLimit (token:string) =
-    if token.Length < maxLineLength then
-        [token]
-    else
-        let first, second = Seq.toList token |> List.splitAt maxLineLength
-        let mutable retval = [String.Concat first]
+    let rec splitTokenAtLimit maxLength (token:string) =
+        if maxLength = 0 then
+            invalidArg "maxLength" "can't be zero"
 
-        if second.Length > maxLineLength then
-            retval <- String.Concat second |> splitTokenAtLimit |> List.append retval
+        if token.Length < maxLength then
+            [token]
         else
-            retval <- [String.Concat second] |> List.append retval
+            let first, second = Seq.toList token |> List.splitAt maxLength
+            let mutable retval = [String.Concat first]
+
+            if second.Length > maxLength then
+                retval <- String.Concat second |> splitTokenAtLimit maxLength |> List.append retval
+            else if second.Length > 0 then
+                retval <- [String.Concat second] |> List.append retval
+
+            retval
+
+    /// Maps tokens to a list of strings with each element shorter than length.
+    let makeLimitedStrings maxLength tokens =
+        let tokenList = List.collect (splitTokenAtLimit maxLength) tokens
+        let mutable s = ""
+        let mutable newS = ""
+        let mutable retval = []
+
+        for t in tokenList do
+            if s.Length > 0 then
+                newS <- s + " " + t
+            else
+                newS <- t
+
+            if newS.Length <= maxLength then
+                s <- newS
+            else
+                retval <- List.append retval [s]
+                s <- t
+
+        retval <- List.append retval [s]
 
         retval
 
-/// Maps tokens to a list of strings with each element shorter than length.
-let makeLimitedStrings tokens =
-    let tokenList = List.collect splitTokenAtLimit tokens
-    let mutable s = ""
-    let mutable newS = ""
-    let mutable retval = []
+    let longestLine (lines:string list) =
+        let mutable length = 0
 
-    for t in tokenList do
-        if s.Length > 0 then
-            newS <- s + " " + t
-        else
-            newS <- t
+        for line in lines do
+            if line.Length > length then
+                length <- line.Length
 
-        if newS.Length <= maxLineLength then
-            s <- newS
-        else
-            retval <- List.append retval [s]
-            s <- t
+        length
 
-    retval <- List.append retval [s]
+    let wrapLine line length =
+        let retval = "| " + line
+        let multi = length - retval.Length + 2
+        retval + String.replicate multi " " + " |"
 
-    retval
+    let topBottomBorder length =
+        "+" + String.replicate (length + 2) "-" + "+"
 
-let longestLine (lines:string list) =
-    let mutable length = 0
+    let makeSignString maxLength (args:string list) =
+        let lines = makeLimitedStrings maxLength args
+        let length = longestLine lines
+        let mutable retval = topBottomBorder length + "\n"
+        retval <- retval + wrapLine " " length + "\n"
 
-    for line in lines do
-        if line.Length > length then
-            length <- line.Length
+        for line in lines do
+            retval <- retval + wrapLine line length + "\n"
 
-    length
+        retval <- retval + wrapLine " " length + "\n"
+        retval + topBottomBorder length
 
-let wrapLine line length =
-    let retval = "| " + line
-    let multi = length - retval.Length + 2
-    retval + String.replicate multi " " + " |"
 
-let topBottomBorder length =
-    "+" + String.replicate (length + 2) "-" + "+"
+module Combinator =
+    let maxLineLength =
+        ConsoleIO.getConsoleWidth - 4
 
-let makeSignString (args:string list) =
-    let lines = makeLimitedStrings args
-    let length = longestLine lines
-    let mutable retval = topBottomBorder length + "\n"
-    retval <- retval + wrapLine " " length + "\n"
+    let makeSign (args:string list) =
+        StringOperations.makeSignString maxLineLength args
 
-    for line in lines do
-        retval <- retval + wrapLine line length + "\n"
-
-    retval <- retval + wrapLine " " length + "\n"
-    retval + topBottomBorder length
 
 [<EntryPoint>]
 let main args =
-    printfn "%s" <| makeSignString (Seq.toList args)
+    printfn "%s" <| Combinator.makeSign (Seq.toList args)
     0
